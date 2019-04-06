0e2c73c88aadf34e50e2a6e9c7bf6eb2ae4d4536
Akihiro <kouen.xyz1@gmail.com>
2019-04-06 10:55:28 +0000

    migrateまで実施

---
+++ db/migrate/20190404233314_create_task_lists.rb
@@ -0,0 +1,19 @@
+class CreateTaskLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :task_lists do |t|
+      t.integer :status
+      t.string :todo
+      t.string :memo
+      t.string :priority
+      t.integer :context
+      t.date :start
+      t.date :end
+      t.float :budget_h
+      t.float :result_h
+      t.integer :budget_p
+      t.integer :result_p
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/initial_task_list.csv') do |row|
+  unless TaskList.find_by(id: row[0])
+    TaskList.create(
+      id: row[0],
+      name: row[1]
+      )
+  end
+end
