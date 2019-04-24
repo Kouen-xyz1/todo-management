2376059a15dc388bf7e6d924564e8e84ec15a6f9
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-04-24 23:34:26 +0900

    rollback

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,2 @@
 $(document).ready(function() {
-  console.log("ready");
-  $(".todo-edit").hide();
-  
-  $(".todo-show").click(function () {
-    console.log("todo-show clicked");
-    $(".todo-show").hide();
-    $(".todo-edit").show();
-  });
-  
-  $(".todo-edit").click(function () {
-    console.log("todo-edit clicked");
-    $(".todo-show").show();
-    $(".todo-edit").hide();
-  });
 });
