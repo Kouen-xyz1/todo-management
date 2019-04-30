b9f09286b6b5b3661c8e972afd576d0015bf6176
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-04-30 22:10:45 +0900

    タスク追加を一覧画面からできるように修正

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,4 +6,21 @@ $(document).ready(function() {
   $(".form-control").change(function() {
     $('#task-form').submit();
   });
+  
+  setToday()
+  
+  $("#new-task").click(function() {
+    $("#new-task-form").submit();
+  });
 });
+
+function setToday() {
+  console.log("setToday")
+  var today = new Date();
+  today.setDate(today.getDate());
+  var yyyy = today.getFullYear();
+  var mm = ("0"+(today.getMonth()+1)).slice(-2);
+  var dd = ("0"+today.getDate()).slice(-2);
+  var todayStr = yyyy+'-'+mm+'-'+dd;
+  $('.today').attr('value', todayStr);
+}
