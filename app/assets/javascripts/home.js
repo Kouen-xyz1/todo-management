b166004552eb0bf372b5175dcd22cf267746dbb3
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-04-30 21:21:23 +0900

    フォームを変更したら自動で保存する

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   setTimeout(function() {
     $(".alert").fadeOut("slow")
   }, 1000);
+  
+  $(".form-control").change(function() {
+    $('#task-form').submit();
+  });
 });
