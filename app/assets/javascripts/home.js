74ee332c4ac705055e4c303967220c9c005fb445
Akihiro <kouen.xyz1@gmail.com>
2019-05-01 14:12:54 +0000

    保存シマシタを表示

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+  setTimeout(function() {
+    $(".alert").fadeOut("slow")
+  }, 1000);
+  
   console.log("ready");
+  $(".form-control").change(function() {
+    $(this).parent('div').parent('form').submit();
+  console.log("change");
+  });
 });
